8b675ff4faf55c35f3493cbc1b98cc11401b2a0f
its-sagar <sagarsinghrajput.india@gmail.com>
2024-12-04 11:50:04 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+// Function to trigger resume download
+document.getElementById('download-btn').addEventListener('click', function () {
+  const resumePath = 'resume.pdf'; // Replace with the actual file path
+  const link = document.createElement('a');
+  link.href = resumePath;
+  link.download = 'Sagar_Resume.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
+  }
+
+  
+}
